b5aa1c1cda27abc5a73d413b093bd9429fb4012a
jluisrcardenas <jluisrcardenas@gmail.com>
2021-03-11 16:44:01 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+//console.log("Hola Mundo");
+
+const express = require('express');
+const app = express();
+
+/**
+ * Verbos HTTP
+ * GET
+ * POST
+ * PATCH
+ * PUT
+ * DELETE
+ */
+
+app.get("/",(req, res, next) =>{
+    res.status(200);
+    res.send("Bienvenido");
+});
+
+app.listen(3000,() => {
+    console.log("Server is running...");
+});
