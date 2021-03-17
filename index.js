ea014b16be0180159e118dc870e3224dcab5e3e7
jluisrcardenas <jluisrcardenas@gmail.com>
2021-03-17 15:38:48 -0600

    NodeJs 00 changes

--- index.js
+++ index.js
@@ -14,9 +14,15 @@ const app = express();
 
 app.get("/",(req, res, next) =>{
     res.status(200);
-    res.send("Bienvenido");
+    res.send("Bienvenido al servidor");
 });
 
-app.listen(3000,() => {
+app.get("/:name",  (req, res, next)=>{
+    req.params.name;
+    res.status(200);
+    res.send("Hola, " + req.params.name);
+});
+
+app.listen(process.env.PORT || 3000,() => {
     console.log("Server is running...");
 });
