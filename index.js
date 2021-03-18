5d8e990f7bb6a34273eb51ddca3ca96ab1b74594
jluisrcardenas <jluisrcardenas@gmail.com>
2021-03-17 19:03:11 -0600

    pokemon id and name functions

--- index.js
+++ index.js
@@ -18,14 +18,33 @@ app.get('/',(req, res, next) =>{
     res.send("Bienvenido al Pokedex");
 });
 
-app.get('/pokemon',  (req, res, next)=>{
+app.get('/pokemon/all',  (req, res, next)=>{
     res.status(200);
     res.send(pokemon);
 });
 
-app.get('/pokemon/:id',  (req, res, next)=>{
-    res.status(200);
-    res.send(pokemon[req.params.id - 1]);
+app.get('/pokemon/:id([0-9]{1,3})',  (req, res, next)=>{
+    const id = req.params.id - 1;
+    if(id >= 0 && id <= 150){
+        res.status(200);
+        res.send(pokemon[req.params.id - 1]);
+    } else{
+        res.status(404);
+        res.send("Pokemon no encontrado");
+    }
+    
+});
+
+app.get('/pokemon/:name',  (req, res, next)=>{
+    const name = req.params.name;
+    for (let i = 0; i < pokemon.length; i++) {
+        if (name == pokemon[i].name) {
+            res.status(200);
+            res.send(pokemon[i]);
+        }
+    }
+    res.status(404);
+    res.send("Pokemon no encontrado");
 });
 
 app.listen(process.env.PORT || 3000,() => {
