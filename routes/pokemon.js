9dbf38b7d9f6dceb71a9a35c55fbf90c6df784cc
jluisrcardenas <jluisrcardenas@gmail.com>
2021-04-13 20:55:52 -0500

    POST en pokemon

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -3,21 +3,32 @@ const pokemon = express.Router();
 //const pk = require('../pokedex.json').pokemon;
 const db = require('../config/database');
 
-pokemon.post('/', (req, res, next)=>{
-    return res.status(200).send(req.body);
+pokemon.post('/', async (req, res, next)=>{
+    const {pok_name, pok_height, pok_weight, pok_base_experience} = req.body;
+    if(pok_name && pok_height && pok_weight && pok_base_experience){
+        let query = "INSERT INTO pokemon(pok_name, pok_height, pok_weight, pok_base_experience)";
+        query +=  ` VALUES ('${pok_name}',${pok_height},${pok_weight},${pok_base_experience})`;
+        const  rows = await db.query(query);
+        console.log(rows);
+        if(rows.affectedRows==1){
+            return res.status(201).json({code: 201, message: "Pokemon insertado correctamente"});
+        }
+        return res.status(500).json({code: 500, message: "Ocurrio un error"});
+    }
+    return res.status(500).json({code: 500, message: "Campos incompletos"});
 });
 
 pokemon.get('/', async (req, res, next)=>{
     const pk = await db.query("SELECT * FROM pokemon");
-    return res.status(200).json({code: 1, message: pk});
+    return res.status(200).json({code: 200, message: pk});
 });
 
 pokemon.get('/:id([0-9]{1,3})', async (req, res, next)=>{
     const pk = await db.query("SELECT * FROM pokemon");
     const id = req.params.id - 1;
     (id >= 0 && id <= 722) ?
-        res.status(200).json({code: 1, message: pk[req.params.id - 1]}) :
-        res.status(404).json({code: 1, message: "Pokemon no encontrado"});
+        res.status(200).json({code: 200, message: pk[req.params.id - 1]}) :
+        res.status(404).json({code: 404, message: "Pokemon no encontrado"});
 });
 
 pokemon.get('/:name([A-Za-z]+)', async (req, res, next)=>{
@@ -28,8 +39,8 @@ pokemon.get('/:name([A-Za-z]+)', async (req, res, next)=>{
         return (p.pok_name.toUpperCase() == name.toUpperCase()) && p;
     });
     (pkm.length > 0) ? 
-        res.status(200).json({code: 1, message: pkm}) : 
-        res.status(404).json({code: 1, message: "Pokemon no encontrado"});
+        res.status(200).json({code: 200, message: pkm}) : 
+        res.status(404).json({code: 404, message: "Pokemon no encontrado"});
 });
 
 
