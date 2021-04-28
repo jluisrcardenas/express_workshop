573e0ab6e9dae4f10ba8a548d17222a0206863bf
jluisrcardenas <jluisrcardenas@gmail.com>
2021-04-27 19:47:08 -0500

    Actividad 13. Implementando JWT

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-//console.log("Hola Mundo");
-
+//Dependences
 const morgan = require('morgan');
 const express = require('express');
 const app = express();
-const pokemon = require('./routes/pokemon')
-const user = require('./routes/user')
+//Routers
+const pokemon = require('./routes/pokemon');
+const user = require('./routes/user');
+//Middleware
+const auth = require('./middleware/auth');
+const notFound = require('./middleware/notFound');
+const index = require('./middleware/index');
 
 /**
  * Verbos HTTP
@@ -15,21 +19,17 @@ const user = require('./routes/user')
  * DELETE
  */
 
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 
-app.get('/',(req, res, next) =>{
-    return res.status(200).json({ code: 1, message: "Bienvenido al Pokedex"});
-});
-
+app.get('/',index);
+app.use("/user", user);
+app.use(auth);
 app.use("/pokemon", pokemon);
 
-app.use("/user", user);
-
-app.use((req, res, next)=>{
-    return res.status(404).json({ code:404, message: "URL no encontrada" });
-});
+app.use(notFound);
 
 app.listen(process.env.PORT || 3000,() => {
     console.log("Server is running...");
